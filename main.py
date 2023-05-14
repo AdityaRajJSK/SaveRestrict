db7c8330d1c5dce0b68b0f7a6142710965f14cbb
AdityaRajJSK <73116892+AdityaRajJSK@users.noreply.github.com>
2023-05-14 11:55:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pyrogram
+
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant, InviteHashExpired
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
